3794c069495184eed48ce5a87e19f52705e8f58f
Dara Ferreira <dara-ingrid@hotmail.com>
2023-04-04 11:57:02 -0300

    Inclui alertas e mensagens

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,7 +4,7 @@ from usuarios.forms import LoginForms, CadastroForms
 
 from django.contrib.auth.models import User
 
-from django.contrib import auth
+from django.contrib import auth, messages
 
 def login(request):
     form = LoginForms()
@@ -24,8 +24,10 @@ def login(request):
 
         if usuario is not None:
             auth.login(request, usuario)
+            messages.success(request, f"{nome} logado com sucesso!")
             return redirect('index')
         else:
+            messages.error(request, "Erro ao efetuar login!")
             return redirect('login')
 
     return render(request, "usuarios/login.html", {"form": form}) 
@@ -38,6 +40,7 @@ def cadastro(request):
 
         if form.is_valid():
             if form["senha_1"].value() != form["senha_2"].value():
+                messages.error(request, "Senhas não são iguais!")
                 return redirect('cadastro')
             
             nome=form["nome_cadastro"].value()
@@ -45,6 +48,7 @@ def cadastro(request):
             senha=form["senha_1"].value()
 
             if User.objects.filter(username=nome).exists():
+                messages.error(request, "Usuário já cadastrado!")
                 return redirect('cadastro')
 
             usuario=User.objects.create_user(
@@ -53,6 +57,7 @@ def cadastro(request):
                 password=senha
             )
             usuario.save()
+            messages.success(request, "Cadastro realizado com sucesso!")
             return redirect('login')
 
     return render(request, "usuarios/cadastro.html", {"form": form}) 
